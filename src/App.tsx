1caea1edc62e5ac8c6899cf7121bc90532f6bcc1
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-11-29 18:11:17 -0300

    feat: insert themeprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/theme/default";
+
 export function App() {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div></div>
+    </ThemeProvider>
+  );
 }
