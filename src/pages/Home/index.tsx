7d2e6b724ffa0c1397318d9bb6ea6b714905d7f4
Gabriel Gomes <gabrielgomes98@gmail.com>
2022-12-01 09:41:05 -0300

    feat: using useForm and register

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
 import { Play } from 'phosphor-react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+
 import {
   CountDownContainer,
   FormContainer,
@@ -10,15 +13,22 @@ import {
 } from './styles'
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleCreateNewCycle(data: any) {}
+
+  const isSubtmitDisabled = !watch('task')
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
           />
           <datalist id="task-suggestions">
             <option value="Projeto 1" />
@@ -34,6 +44,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutes', { valueAsNumber: true })}
           />
           <span>minutos.</span>
         </FormContainer>
@@ -44,7 +55,7 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </CountDownContainer>
-        <StartCountDownButton disabled type="submit">
+        <StartCountDownButton disabled={isSubtmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountDownButton>
